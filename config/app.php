02204fb9fd14a31142b3993f2e7ac4281788583b
Budi Hermawan Saka <budihermawan@gmail.com>
2023-04-16 15:03:17 +0800

    update

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Arcanedev\LogViewer\LogViewerServiceProvider::class,
+
 
         /*
          * Package Service Providers...
