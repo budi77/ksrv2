77ecfd9f6b56d2053a0610c4add8a0d6545e0edf
budi77 <budihermawan@gmail.com>
2023-07-20 14:52:38 +0800

    patriotik

---
+++ database/migrations/2023_07_20_064547_create_patriotiks_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patriotiks', function (Blueprint $table) {
+            $table->id();
+            $table->string('penyertaan');
+            $table->string('nama');
+            $table->foreignId('department_id');
+            $table->string('alamat');
+            $table->string('telefon')->nullable();
+            $table->string('emel')->nullable();
+            $table->string('tajuk_lagu')->nullable();
+            $table->string('penyanyi')->nullable();
+            $table->string('pautan')->nullable();
+            $table->string('extra1')->nullable();
+            $table->string('extra2')->nullable();
+            $table->string('extra3')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patriotiks');
+    }
+};
