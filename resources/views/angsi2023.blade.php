a334edfcf3c00ad1e23cc2d7d5be828d60f53bd5
budi77 <budihermawan@gmail.com>
2023-10-04 12:38:13 +0800

    update

--- resources/views/angsi2023.blade.php
+++ resources/views/angsi2023.blade.php
@@ -12,16 +12,17 @@
        <div class="auth-page-wrapper py-5 d-flex justify-content-center align-items-center min-vh-100">
 
         <!-- auth-page content -->
-        <div class="auth-page-content overflow-hidden p-0">
+        {{-- <div class="auth-page-content overflow-hidden p-0"> --}}
             <div class="container-fluid">
                 <div class="row justify-content-center">
                     <div class="col-xl-4 text-center">
+                        <h4 class="pt-4 font-danger fw-bold">PENDAFTARAN TELAH DITUTUP!</h4>
+
                         <div class="position-relative">
-                            <img src="{{ URL::asset('assets/images/angsi.png') }}" alt="" class="img-fluid error-500-img error-img" />
+                            <img src="{{ URL::asset('assets/images/angsi.png') }}" alt="" class="img-fluid " />
                             {{-- <h1 class="title text-muted">PEDAFTARAN TELAH DITUTUP</h1> --}}
                         </div>
                         <div>
-                            <h4 class="pt-4 font-danger fw-bold">PENDAFTARAN TELAH DITUTUP!</h4>
                             {{-- <p class="text-muted w-75 mx-auto">Server Error 500. We're not exactly sure what happened, but our servers say something is wrong.</p> --}}
                             {{-- <a href="index" class="btn btn-success"><i class="mdi mdi-home me-1"></i>Back to home</a> --}}
                         </div>
@@ -30,7 +31,7 @@
                 <!-- end row -->
             </div>
             <!-- end container -->
-        </div>
+        {{-- </div> --}}
         <!-- end auth-page content -->
     </div>
     <!-- end
--- storage/framework/views/905230fc217ceba16fb203a3ef9064d891e73d4b.php
+++ storage/framework/views/905230fc217ceba16fb203a3ef9064d891e73d4b.php
@@ -12,16 +12,17 @@
        <div class="auth-page-wrapper py-5 d-flex justify-content-center align-items-center min-vh-100">
 
         <!-- auth-page content -->
-        <div class="auth-page-content overflow-hidden p-0">
+        
             <div class="container-fluid">
                 <div class="row justify-content-center">
                     <div class="col-xl-4 text-center">
+                        <h4 class="pt-4 font-danger fw-bold">PENDAFTARAN TELAH DITUTUP!</h4>
+
                         <div class="position-relative">
-                            <img src="<?php echo e(URL::asset('assets/images/angsi.png')); ?>" alt="" class="img-fluid error-500-img error-img" />
+                            <img src="<?php echo e(URL::asset('assets/images/angsi.png')); ?>" alt="" class="img-fluid " />
                             
                         </div>
                         <div>
-                            <h4 class="pt-4 font-danger">PENDAFTARAN TELAH DITUTUP!</h4>
                             
                             
                         </div>
@@ -30,7 +31,7 @@
                 <!-- end row -->
             </div>
             <!-- end container -->
-        </div>
+        
         <!-- end auth-page content -->
     </div>
     <!-- end
