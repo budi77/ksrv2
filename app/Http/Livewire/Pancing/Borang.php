3983c12c0bfc5419e0e0a122d5125c2bc87e49fd
Budi Hermawan Saka <budihermawan@gmail.com>
2023-11-02 21:45:21 +0800

    update

--- app/Http/Livewire/Pancing/Borang.php
+++ app/Http/Livewire/Pancing/Borang.php
@@ -14,9 +14,38 @@ class Borang extends Component
     use LivewireAlert;
     use WithFileUploads;
 
-    public $departments, $nama,$nokp, $jantina, $bahagian, $ahli, $no_tel, $tarikh, $jumlah_bayaran, $lampiran ;
+    public $departments, $nama,$nokp, $jantina, $bahagian, $ahli, $no_tel, $tarikh, $jumlah_bayaran, $lampiran, $saiz ;
     public bool $checked = false;
     public bool $disabled = true;
+
+    protected $rules = [
+        'nama' => 'required',
+        'nokp' => 'required',
+        'no_tel' => 'required',
+        'jantina' => 'required',
+        'bahagian' => 'required',
+        'ahli' => 'required',
+        'tarikh' => 'required',
+        'jumlah_bayaran' => 'required',
+        'saiz' => 'required',
+        'lampiran' => 'required',
+     
+    ];
+
+    
+    // protected $messages = [
+    //     'nama.required' => 'Sila masukkan nama',
+    //     'kp.required' => 'Sila masukkan No Kad Pengenalan',
+    //     'tel.required' => 'Sila masukkan No Telefon',
+    //     'emel.required' => 'Sila masukkan Emel',
+    //     'department_id.required' => 'Sila masukkan Bahagian',
+    //     'nama_waris.required' => 'Sila masukkan Nama Waris',
+    //     'tel_waris.required' => 'Sila masukkan No Kecemasan Untuk Dihubungi',
+    //     'pengangkutan.required' => 'Sila masukkan Jenis Pengangkutan',
+    //     'penyakit.required' => 'Sila pilih jika ada penyakit',
+    //     'saiz.required' => 'Sila pilih saiz baju',
+       
+    // ];
 
     public function render()
     {
@@ -36,6 +65,55 @@ class Borang extends Component
         }
 
     }
+
+    public function store()
+    {
+
+        // $total = Angsi::count();
+
+        // if($total >= $this->kouta)
+        // {
+        //     abort(503);
+        // }
+
+        $this->validate();
+
+        $name = md5($this->lampiran . microtime()).'.'.$this->lampiran->extension();
+
+        $this->lampiran->storeAs('public', $name);
+
+
+        Pancing::create([
+            'nama' => $this->nama,
+            'no_kp' => $this->nokp,
+            'ext2' => $this->no_tel,
+            'jantina' => $this->jantina,
+            'department_id' => $this->bahagian,
+            'ahli' => $this->ahli,
+            'tarikh_bayaran' => $this->tarikh,
+            'jumlah_bayaran' => $this->jumlah_bayaran,
+            'lampiran_bayaran' => $this->lampiran,
+            'ext1' => $this->saiz,
+            
+        ]);
+
+
+
+        $this->resetExcept('bahagian');
+        
+        // $this->alert('success', 'Berjaya!');
+
+        // $this->isOpen = false;
+        // $this->isCompleted = true;
+
+
+        
+
+
+    }
+
+
+
 
     
 }
--- storage/framework/views/1d5197d23a38a06b0cd16b4c6ff86b84c93260a1.php
+++ storage/framework/views/1d5197d23a38a06b0cd16b4c6ff86b84c93260a1.php
@@ -1,12 +1,9 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    
     <div class="container">
         <div>
 
-            {{-- <picture>
-                <source srcset="sourceset" type="image/svg+xml">
-                <img src="{{ URL::asset('assets/images/angsi_header.png') }}" class="img-fluid" alt="image desc">
-            </picture> --}}
+            
 
 
         </div>
@@ -16,31 +13,31 @@
             <div class="card">
                 <div class="row g-0">
                     <div class="col-md-6">
-                        <img class="rounded-start img-fluid" src="{{asset('assets/images/pancing2023.jpg')}}" alt="Card image">
-                        <img class="rounded-start img-fluid" src="{{asset('assets/images/bankislam.jpg')}}" alt="Card image">
+                        <img class="rounded-start img-fluid" src="<?php echo e(asset('assets/images/pancing2023.jpg')); ?>" alt="Card image">
+                        <img class="rounded-start img-fluid" src="<?php echo e(asset('assets/images/bankislam.jpg')); ?>" alt="Card image">
                         <p></p>
                         <p></p>
                     </div>
                     <div class="col-md-6">
                         <div class="card-header">
-                        <a name="" id="" class="btn btn-secondary" href="{{url('storage/' . 'syarat.pdf')}}" target="_blank" role="button">Syarat & Maklumat Umum Angsi</a>
-                        <a name="" id="" class="btn btn-secondary" href="{{url('storage/' . 'tentatif.pdf')}}" target="_blank" role="button">Tentatif Program Angsi</a>
+                        <a name="" id="" class="btn btn-secondary" href="<?php echo e(url('storage/' . 'syarat.pdf')); ?>" target="_blank" role="button">Syarat & Maklumat Umum Angsi</a>
+                        <a name="" id="" class="btn btn-secondary" href="<?php echo e(url('storage/' . 'tentatif.pdf')); ?>" target="_blank" role="button">Tentatif Program Angsi</a>
                             <h5 class="card-title mb-0 pt-3 ">BORANG PENDAFTARAN</h5>
                         </div>
                         <div class="card-body">
 
-                            {{-- @if($isOpen) --}}
+                            
 
                             <div>
-                                @if ($errors->any())
+                                <?php if($errors->any()): ?>
                                     <div class="alert alert-danger">
                                         <ul>
-                                            @foreach ($errors->all() as $error)
-                                                <li>{{ $error }}</li>
-                                            @endforeach
+                                            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                <li><?php echo e($error); ?></li>
+                                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                         </ul>
                                     </div>
-                                @endif
+                                <?php endif; ?>
 
                                 <div class="mb-3">
                                 <label for="" class="form-label">Nama Penuh :</label>
@@ -74,20 +71,16 @@
                                     </div>
                                 </div>
 
-                                {{-- <div class="mb-3">
-                                <label for="" class="form-label">Emel</label>
-                                <input type="text"
-                                    class="form-control" name="" id="" aria-describedby="helpId" placeholder="" wire:model="emel">
-                                </div> --}}
+                                
 
 
                                 <div class="mb-3">
                                     <label for="" class="form-label">Bahagian</label>
                                     <select class="form-select" name="" id="" wire:model="bahagian">
                                         <option selected>-- Pilih --</option>
-                                        @foreach($departments as $department)
-                                        <option value="{{ $department->id }}">{{ $department->name }}</option>
-                                        @endforeach
+                                        <?php $__currentLoopData = $departments; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $department): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                        <option value="<?php echo e($department->id); ?>"><?php echo e($department->name); ?></option>
+                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                         
                                     </select>
                                 </div>
@@ -137,7 +130,21 @@
                                     <div class="col-6">
                                         <div class="mb-3">
                                         <label for="" class="form-label">Tarikh bayaran</label>
-                                         <x-date-picker id="date" wire:model="tarikh" autocomplete="off" value="" />
+                                         <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.date-picker','data' => ['id' => 'date','wire:model' => 'tarikh','autocomplete' => 'off','value' => '']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('date-picker'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['id' => 'date','wire:model' => 'tarikh','autocomplete' => 'off','value' => '']); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
 
                                         </div>
                                     </div>
@@ -151,53 +158,17 @@
                                 </div>
 
                                 
-                                {{-- <div class="mb-3">
-
-                                    <div>
-                                        <label for="" class="form-label">Pengangkutan</label>
-
-                                    </div>
-
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="pengangkutan" id="" value="Kenderaan Sendiri" wire:model="pengangkutan">
-                                        <label class="form-check-label" for="">Kenderaan Sendiri</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="pengangkutan" id="" value="Menaiki Bas SPRM" wire:model="pengangkutan">
-                                        <label class="form-check-label" for="">Menaiki Bas SPRM</label>
-                                    </div>
-
-                                </div> --}}
+                                
 
 
-                                {{-- <div class="mb-3">
+                                
 
-                                    <div>
-                                        <label for="" class="form-label">Adakah mempunyai sebarang penyakit kronik?</label>
-
-                                    </div>
-
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="penyakit" id="" value="Ya" wire:model="penyakit">
-                                        <label class="form-check-label" for="">Ya</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="penyakit" id="" value="Tidak" wire:model="penyakit">
-                                        <label class="form-check-label" for="">Tidak</label>
-                                    </div>
-
-                                </div> --}}
-
-                                {{-- <div class="mb-3">
-                                <label for="" class="form-label">Jika jawapan di atas adalah YA, sila nyatakan </label>
-                                <input type="text"
-                                    class="form-control" name="" id="" aria-describedby="helpId" placeholder="" wire:model="jenis_penyakit">
-                                </div> --}}
+                                
 
                                 <div class="mb-3">
                                   <label for="" class="form-label">Muatnaik resit pembayaran</label>
                                   <input type="file" class="form-control" name="" id="" placeholder="" aria-describedby="fileHelpId" wire:model="lampiran">
-                                  {{-- <div id="fileHelpId" class="form-text">Help text</div> --}}
+                                  
                                 </div>
 
                                 
@@ -213,26 +184,17 @@
                             <hr>
 
                               <div class="text-end pt-3">
-                                <button type="button" class="btn btn-success bg-gradient waves-effect waves-light" wire:click="store" {{ $disabled ? 'disabled' : '' }}>Hantar</button>
+                                <button type="button" class="btn btn-success bg-gradient waves-effect waves-light" wire:click="store" <?php echo e($disabled ? 'disabled' : ''); ?>>Hantar</button>
                             </div>
                             
                            
-                            {{-- @elseif($isClosed) --}}
+                            
 
                             <!-- Warning Alert -->
-                            {{-- <div class="alert bg-warning border-warning text-white" role="alert">
-                                <strong>Untuk makluman</strong> - Pendaftaran telah ditutup!
-                            </div>
-
-                            @elseif($isCompleted)
-
-                            <div class="alert alert-secondary alert-dismissible bg-secondary text-white alert-label-icon fade show" role="alert">
-                                <i class="ri-check-double-line label-icon"></i><strong>Berjaya</strong> - Permohonan telah diterima!
-                                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
-                            </div> --}}
+                            
 
 
-                            {{-- @endif --}}
+                            
 
                           
                             
@@ -256,12 +218,12 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <img class="rounded-start img-fluid" src="{{asset('assets/images/size.jpg')}}" alt="">
+                        <img class="rounded-start img-fluid" src="<?php echo e(asset('assets/images/size.jpg')); ?>" alt="">
 
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                        {{-- <button type="button" class="btn btn-primary">Save</button> --}}
+                        
                     </div>
                 </div>
             </div>
@@ -280,3 +242,4 @@
     </div>
 </div>
 
+<?php /**PATH C:\laragon\www\ksrv2\resources\views/livewire/pancing/borang.blade.php ENDPATH**/ ?>
