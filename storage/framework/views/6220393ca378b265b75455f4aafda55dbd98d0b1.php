eed16a01830884b237cd21cffa159527761b5244
budi77 <budihermawan@gmail.com>
2023-12-19 15:32:56 +0800

    kpb

--- storage/framework/views/6220393ca378b265b75455f4aafda55dbd98d0b1.php
+++ storage/framework/views/6220393ca378b265b75455f4aafda55dbd98d0b1.php
@@ -19,22 +19,21 @@ Invoice Details
                             <div class="flex-grow-1">
                                <img src="<?php echo e(URL::asset('assets/images/KSR.png')); ?>" alt="" height="90">
                                
-                                <div class="mt-sm-5 mt-4">
-                                    <h6 class="text-muted text-uppercase fw-semibold">Address</h6>
-                                    <p class="text-muted mb-1" id="address-details">California, United States</p>
-                                    <p class="text-muted mb-0" id="zip-code"><span>Zip-code:</span> 90201</p>
+                                <div class="mt-sm-2 mt-2">
+                                    <h6 class="text-dark text-uppercase fw-semibold">KELAB SUKAN DAN REKREASI (KSR) SPRM</h6>
+                                    <p class="text-muted mb-1" id="address-details">2, Lebuh Wawasan, Presint 7, </p>
+                                    <p class="text-muted mb-0" id="zip-code">62250 Putrajaya, Wilayah Persekutuan Putrajaya</p>
                                 </div>
                             </div>
                             <div class="flex-shrink-0 mt-sm-0 mt-3">
-                                <h6><span class="text-muted fw-normal">Legal
-                                        Registration No:</span>
-                                    <span id="legal-register-no">987654</span>
+                                <h3><span class="text-dark fw-bold">RESIT RASMI</span>
+                                    
+                                </h3>
+                                <h6><span class="text-muted fw-normal">No Resit:</span>
+                                    <span id="email">123454/2023</span>
                                 </h6>
-                                <h6><span class="text-muted fw-normal">Email:</span>
-                                    <span id="email">velzon@themesbrand.com</span>
-                                </h6>
-                                <h6><span class="text-muted fw-normal">Website:</span> <a href="https://themesbrand.com/" class="link-primary" target="_blank" id="website">www.themesbrand.com</a></h6>
-                                <h6 class="mb-0"><span class="text-muted fw-normal">Contact No: </span><span id="contact-no"> +(01) 234 6789</span></h6>
+                                
+                                <h6 class="mb-0"><span class="text-muted fw-normal">Tarikh: </span><span> 19/12/2023</span></h6>
                             </div>
                         </div>
                     </div>
@@ -44,25 +43,22 @@ Invoice Details
                 <div class="col-lg-12">
                     <div class="card-body p-4">
                         <div class="row g-3">
-                            <div class="col-lg-3 col-6">
-                                <p class="text-muted mb-2 text-uppercase fw-semibold">Invoice No</p>
-                                <h5 class="fs-14 mb-0">#VL<span id="invoice-no">25000355</span></h5>
+                            <div class="col-lg-4 col-6">
+                                <p class="text-muted mb-2 text-uppercase fw-semibold">Diterima daripada:</p>
+                                <h5 class="fs-14 mb-0 text-uppercase">Budi Hermawan</h5>
                             </div>
                             <!--end col-->
-                            <div class="col-lg-3 col-6">
-                                <p class="text-muted mb-2 text-uppercase fw-semibold">Date</p>
-                                <h5 class="fs-14 mb-0"><span id="invoice-date">23 Nov, 2021</span> <small class="text-muted" id="invoice-time">02:36PM</small></h5>
+                            <div class="col-lg-4 col-6">
+                                <p class="text-muted mb-2 text-uppercase fw-semibold">Ringgit Malaysia</p>
+                                <h5 class="fs-14 mb-0">LIMA RATUS LIMA PULUH RINGGIT SAHAJA</h5>
                             </div>
                             <!--end col-->
-                            <div class="col-lg-3 col-6">
-                                <p class="text-muted mb-2 text-uppercase fw-semibold">Payment Status</p>
-                                <span class="badge badge-soft-success fs-11" id="payment-status">Paid</span>
+                            <div class="col-lg-4 col-6">
+                                <p class="text-muted mb-2 text-uppercase fw-semibold">untuk bayaran</p>
+                                <h5 class="fs-14 mb-0">YURAN KSR UNTUK BAHAGIAN PENGURUSAN REKOD DAN MAKLUMAT UNTUK BULAN JULAI 2023 SEPERTI PENAMA DIBAWAH</h5>
                             </div>
                             <!--end col-->
-                            <div class="col-lg-3 col-6">
-                                <p class="text-muted mb-2 text-uppercase fw-semibold">Total Amount</p>
-                                <h5 class="fs-14 mb-0">$<span id="total-amount">755.96</span></h5>
-                            </div>
+                            
                             <!--end col-->
                         </div>
                         <!--end row-->
@@ -70,31 +66,7 @@ Invoice Details
                     <!--end card-body-->
                 </div>
                 <!--end col-->
-                <div class="col-lg-12">
-                    <div class="card-body p-4 border-top border-top-dashed">
-                        <div class="row g-3">
-                            <div class="col-6">
-                                <h6 class="text-muted text-uppercase fw-semibold mb-3">Billing Address</h6>
-                                <p class="fw-medium mb-2" id="billing-name">David Nichols</p>
-                                <p class="text-muted mb-1" id="billing-address-line-1">305 S San Gabriel Blvd</p>
-                                <p class="text-muted mb-1"><span>Phone: +</span><span id="billing-phone-no">(123)
-                                        456-7890</span></p>
-                                <p class="text-muted mb-0"><span>Tax: </span><span id="billing-tax-no">12-3456789</span> </p>
-                            </div>
-                            <!--end col-->
-                            <div class="col-6">
-                                <h6 class="text-muted text-uppercase fw-semibold mb-3">Shipping Address</h6>
-                                <p class="fw-medium mb-2" id="shipping-name">David Nichols</p>
-                                <p class="text-muted mb-1" id="shipping-address-line-1">305 S San Gabriel Blvd</p>
-                                <p class="text-muted mb-1"><span>Phone: +</span><span id="shipping-phone-no">(123)
-                                        456-7890</span></p>
-                            </div>
-                            <!--end col-->
-                        </div>
-                        <!--end row-->
-                    </div>
-                    <!--end card-body-->
-                </div>
+                
                 <!--end col-->
                 <div class="col-lg-12">
                     <div class="card-body p-4">
@@ -103,52 +75,48 @@ Invoice Details
                                 <thead>
                                     <tr class="table-active">
                                         <th scope="col" style="width: 50px;">#</th>
-                                        <th scope="col">Product Details</th>
-                                        <th scope="col">Rate</th>
-                                        <th scope="col">Quantity</th>
-                                        <th scope="col" class="text-end">Amount</th>
+                                        <th scope="col">Nama</th>
+                                        <th scope="col">Bahagian</th>
+                                        <th scope="col">Jumlah</th>
+                                        
                                     </tr>
                                 </thead>
                                 <tbody id="products-list">
                                     <tr>
                                         <th scope="row">01</th>
                                         <td class="text-start">
-                                            <span class="fw-medium">Sweatshirt for Men (Pink)</span>
-                                            <p class="text-muted mb-0">Graphic Print Men & Women Sweatshirt</p>
+                                            <span class="fw-medium">Budi Hermawan</span>
                                         </td>
-                                        <td>$119.99</td>
-                                        <td>02</td>
+                                        <td><p class="text-muted mb-0">BAHAGIAN PENGURUSAN REKOD DAN MAKLUMAT</p></td>
+                                        
                                         <td class="text-end">$239.98</td>
                                     </tr>
                                     <tr>
                                         <th scope="row">02</th>
                                         <td class="text-start">
-                                            <span class="fw-medium">Noise NoiseFit Endure Smart Watch</span>
-                                            <p class="text-muted mb-0">32.5mm (1.28 Inch) TFT Color Touch Display</p>
+                                            <span class="fw-medium">Budi Hermawan</span>
                                         </td>
-                                        <td>$94.99</td>
-                                        <td>01</td>
-                                        <td class="text-end">$94.99</td>
+                                        <td><p class="text-muted mb-0">BAHAGIAN PENGURUSAN REKOD DAN MAKLUMAT</p></td>
+                                        
+                                        <td class="text-end">$239.98</td>
                                     </tr>
                                     <tr>
                                         <th scope="row">03</th>
                                         <td class="text-start">
-                                            <span class="fw-medium">350 ml Glass Grocery Container</span>
-                                            <p class="text-muted mb-0">Glass Grocery Container (Pack of 3, White)</p>
+                                            <span class="fw-medium">Budi Hermawan</span>
                                         </td>
-                                        <td>$24.99</td>
-                                        <td>01</td>
-                                        <td class="text-end">$24.99</td>
+                                        <td><p class="text-muted mb-0">BAHAGIAN PENGURUSAN REKOD DAN MAKLUMAT</p></td>
+                                        
+                                        <td class="text-end">$239.98</td>
                                     </tr>
                                     <tr>
                                         <th scope="row">04</th>
                                         <td class="text-start">
-                                            <span class="fw-medium">Fabric Dual Tone Living Room Chair</span>
-                                            <p class="text-muted mb-0">Chair (White)</p>
+                                            <span class="fw-medium">Budi Hermawan</span>
                                         </td>
-                                        <td>$340.00</td>
-                                        <td>01</td>
-                                        <td class="text-end">$340.00</td>
+                                        <td><p class="text-muted mb-0">BAHAGIAN PENGURUSAN REKOD DAN MAKLUMAT</p></td>
+                                        
+                                        <td class="text-end">$239.98</td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -157,25 +125,13 @@ Invoice Details
                         <div class="border-top border-top-dashed mt-2">
                             <table class="table table-borderless table-nowrap align-middle mb-0 ms-auto" style="width:250px">
                                 <tbody>
-                                    <tr>
-                                        <td>Sub Total</td>
-                                        <td class="text-end">$699.96</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Estimated Tax (12.5%)</td>
-                                        <td class="text-end">$44.99</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Discount <small class="text-muted">(VELZON15)</small></td>
-                                        <td class="text-end">- $53.99</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Shipping Charge</td>
-                                        <td class="text-end">$65.00</td>
-                                    </tr>
+                                    
+                                    
+                                    
+                                    
                                     <tr class="border-top border-top-dashed fs-15">
-                                        <th scope="row">Total Amount</th>
-                                        <th class="text-end">$755.96</th>
+                                        <th scope="row">Jumlah Keseluruhan</th>
+                                        <th class="text-end">RM550.00</th>
                                     </tr>
                                 </tbody>
                             </table>
@@ -185,13 +141,11 @@ Invoice Details
                             <!--end table-->
                         </div>
                         <div class="mt-3">
-                            <h6 class="text-muted text-uppercase fw-semibold mb-3">Payment Details:</h6>
-                            <p class="text-muted mb-1">Payment Method: <span class="fw-medium" id="payment-method">Mastercard</span></p>
-                            <p class="text-muted mb-1">Card Holder: <span class="fw-medium" id="card-holder-name">David
-                                    Nichols</span></p>
-                            <p class="text-muted mb-1">Card Number: <span class="fw-medium" id="card-number">xxx xxxx xxxx
-                                    1234</span></p>
-                            <p class="text-muted">Total Amount: <span class="fw-medium" id="">$ </span><span id="card-total-amount">755.96</span></p>
+                            <h6 class="text-muted text-uppercase fw-semibold mb-3">Maklumat Pembayaran:</h6>
+                            <p class="text-muted mb-1">Cara Pembayaran: <span class="fw-medium" id="payment-method">Tunai</span></p>
+                            
+                            
+                            
                         </div>
                         <div class="mt-4">
                             <div class="alert alert-info">
