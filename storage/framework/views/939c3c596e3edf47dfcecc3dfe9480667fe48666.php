1798f6e4c11ac37cb24190da32f185318fb9dd24
budi77 <budihermawan@gmail.com>
2024-09-10 20:25:06 +0800

    contact

--- storage/framework/views/939c3c596e3edf47dfcecc3dfe9480667fe48666.php
+++ storage/framework/views/939c3c596e3edf47dfcecc3dfe9480667fe48666.php
@@ -5,11 +5,11 @@
     <div class="row pt-2">
         <div class="text-center">
 
-            <img src="http://ksrv2.test/assets/images/KSR.png" alt="" class="rounded avatar-xl" >
+            <img src="<?php echo e(URL::asset('assets/images/KSR.png')); ?>" alt="" class="rounded avatar-xl" >
         </div>
 
-    <div class="text-center h4 pt-4">KSR GAMES 2024</div>
-    <p class="text-center h4 text-muted">UITM SHAH ALAM</p>
+    <div class="text-center h4 pt-4">MACC GAMES 2024</div>
+    <!-- <p class="text-center h4 text-muted">UITM SHAH ALAM</p> -->
 
         <!-- Buttons Grid -->
     <div class="d-grid gap-2 pt-2 pb-2" >
