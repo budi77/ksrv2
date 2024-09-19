e053cb66746cac216d580a9f85e7484329b94ecc
budi77 <budihermawan@gmail.com>
2024-09-19 15:11:01 +0800

    btw

--- resources/views/livewire/ksr-games24/portal/btw.blade.php
+++ resources/views/livewire/ksr-games24/portal/btw.blade.php
@@ -114,15 +114,14 @@
             @foreach($fixtures as $fixture)
             @if($fixture->stage == 'Kumpulan')
                 <tr class="text-center">
-                    <td scope="row">{{ $fixture->order }}</td>
-                    <!-- <td>{{ $fixture->stage }}</td> -->
-                    <td>{{ $fixture->ext2 }}</td>
-                    <td>{{ $fixture->ext1 }}</td>
-                    <td>{{ $fixture->court }}</td>
-                    <td>{{ $fixture->contigent1->code }}</td>
-                    <td>{{ $fixture->result1 }}</td>
-                    <td>{{ $fixture->result2 }}</td>
-                    <td>{{ $fixture->contigent2->code }}</td>
+                    <td scope="row">{{ @$fixture->order }}</td>
+                    <td>{{ @$fixture->ext2 }}</td>
+                    <td>{{ @$fixture->ext1 }}</td>
+                    <td>{{ @$fixture->court }}</td>
+                    <td>{{ @$fixture->contigent1->code }}</td>
+                    <td>{{ @$fixture->result1 }}</td>
+                    <td>{{ @$fixture->result2 }}</td>
+                    <td>{{ @$fixture->contigent2->code }}</td>
                     
                 </tr>
             @endif
@@ -160,15 +159,14 @@
             @foreach($fixtures as $fixture)
             @if($fixture->stage == 'Separuh Akhir')
                 <tr class="text-center">
-                    <td scope="row">{{ $fixture->order }}</td>
-                    <!-- <td>{{ $fixture->stage }}</td> -->
-                    <td>{{ $fixture->ext2 }}</td>
-                    <td>{{ $fixture->ext1 }}</td>
-                    <td>{{ $fixture->court }}</td>
-                    <td>{{ $fixture->contigent1->code }}</td>
-                    <td>{{ $fixture->result1 }}</td>
-                    <td>{{ $fixture->result2 }}</td>
-                    <td>{{ $fixture->contigent2->code }}</td>
+                <td scope="row">{{ @$fixture->order }}</td>
+                    <td>{{ @$fixture->ext2 }}</td>
+                    <td>{{ @$fixture->ext1 }}</td>
+                    <td>{{ @$fixture->court }}</td>
+                    <td>{{ @$fixture->contigent1->code }}</td>
+                    <td>{{ @$fixture->result1 }}</td>
+                    <td>{{ @$fixture->result2 }}</td>
+                    <td>{{ @$fixture->contigent2->code }}</td>
                     
                 </tr>
             @endif
@@ -205,15 +203,14 @@
             @foreach($fixtures as $fixture)
             @if($fixture->stage == 'Tempat 3 / 4')
                 <tr class="text-center">
-                    <td scope="row">{{ $fixture->order }}</td>
-                    <!-- <td>{{ $fixture->stage }}</td> -->
-                    <td>{{ $fixture->ext2 }}</td>
-                    <td>{{ $fixture->ext1 }}</td>
-                    <td>{{ $fixture->court }}</td>
-                    <td>{{ $fixture->contigent1->code }}</td>
-                    <td>{{ $fixture->result1 }}</td>
-                    <td>{{ $fixture->result2 }}</td>
-                    <td>{{ $fixture->contigent2->code }}</td>
+                <td scope="row">{{ @$fixture->order }}</td>
+                    <td>{{ @$fixture->ext2 }}</td>
+                    <td>{{ @$fixture->ext1 }}</td>
+                    <td>{{ @$fixture->court }}</td>
+                    <td>{{ @$fixture->contigent1->code }}</td>
+                    <td>{{ @$fixture->result1 }}</td>
+                    <td>{{ @$fixture->result2 }}</td>
+                    <td>{{ @$fixture->contigent2->code }}</td>
                     
                 </tr>
             @endif
@@ -250,16 +247,14 @@
             @foreach($fixtures as $fixture)
             @if($fixture->stage == 'Akhir')
                 <tr class="text-center">
-                    <td scope="row">{{ $fixture->order }}</td>
-                    <!-- <td>{{ $fixture->stage }}</td> -->
-                    <td>{{ $fixture->ext2 }}</td>
-                    <td>{{ $fixture->ext1 }}</td>
-                    <td>{{ $fixture->court }}</td>
-                    <td>{{ $fixture->contigent1->code }}</td>
-                    <td>{{ $fixture->result1 }}</td>
-                    <td>{{ $fixture->result2 }}</td>
-                    <td>{{ $fixture->contigent2->code }}</td>
-                    
+                <td scope="row">{{ @$fixture->order }}</td>
+                    <td>{{ @$fixture->ext2 }}</td>
+                    <td>{{ @$fixture->ext1 }}</td>
+                    <td>{{ @$fixture->court }}</td>
+                    <td>{{ @$fixture->contigent1->code }}</td>
+                    <td>{{ @$fixture->result1 }}</td>
+                    <td>{{ @$fixture->result2 }}</td>
+                    <td>{{ @$fixture->contigent2->code }}</td>
                 </tr>
             @endif
             @endforeach
