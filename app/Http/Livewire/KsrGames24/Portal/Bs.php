85032da6913b7107495cb863e98890ca219a6b8f
budi77 <budihermawan@gmail.com>
2024-09-22 21:09:42 +0800

    update

--- app/Http/Livewire/KsrGames24/Portal/Bs.php
+++ app/Http/Livewire/KsrGames24/Portal/Bs.php
@@ -21,7 +21,7 @@ class Bs extends Component
         $id = $this->sport_id->id;
 
         $a = Contigent::whereHas('grp', function($q) use($id) {
-             $q->where('name', 'A')->where('sport_id', $id)->orderBy('order','ASC');
+             $q->where('name', 'A')->where('sport_id', $id)->orderBy('order');
          })->get()->sortByDesc('bsgoaldifference')->sortByDesc('bspoints');
         $b = Contigent::whereHas('grp', function($q) use($id) {
              $q->where('name', 'B')->where('sport_id', $id)->orderBy('order');
