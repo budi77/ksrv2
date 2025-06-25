ae5bde455b3c2816b546e425a349df933af3c14c
budi77 <budihermawan@gmail.com>
2025-06-26 07:57:42 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/landing', function () {
 
 Route::get('/patriotik2025', \App\Http\Livewire\Patriotik\Index::class)->name('patriotik');
 Route::get('/patriotik2025/berjaya', \App\Http\Livewire\Patriotik\Success::class)->name('patriotik.berjaya');
-Route::get('/patriotik2025', \App\Http\Livewire\Patriotik\Keputusan::class)->name('patriotik');
+//Route::get('/patriotik2025', \App\Http\Livewire\Patriotik\Keputusan::class)->name('patriotik');
 
 Route::get('/semakan', \App\Http\Livewire\Member\Semakan::class)->name('semakan');
 
