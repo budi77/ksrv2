c5abba9a4b8b47e9e873a9a05e202a4e4f5d1d67
budi77 <budihermawan@gmail.com>
2025-07-21 08:45:07 +0800

    pdf

--- resources/views/pdf/patriotik.blade.php
+++ resources/views/pdf/patriotik.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>BORANG PENYERTAAN PERTANDINGAN NYANYIAN LAGU PATRIOTIK SEMPENA SAMBUTAN ULANGTAHUN SPRM KE 56 TAHUN 2023.</title>
+    <title>BORANG PENYERTAAN PERTANDINGAN NYANYIAN LAGU PATRIOTIK SEMPENA SAMBUTAN ULANGTAHUN SPRM KE 58 TAHUN 2025.</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     {{-- <link href="{{ URL::asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" /> --}}
 
@@ -60,10 +60,10 @@
             <td scope="row" style="width:30%" class="fw-bold">ID</td>
             <td>P{{ sprintf('%02d',$result['id']) }}</td>
           </tr> --}}
-          <tr class="">
+          {{-- <tr class="">
             <td scope="row" style="width:30%" class="fw-bold">KATEGORI</td>
             <td>{{ $result['penyertaan'] }}</td>
-          </tr>
+          </tr> --}}
           <tr class="">
             <td scope="row" class="fw-bold">NAMA PESERTA / KUMPULAN</td>
             <td>{{ $result['nama'] }}</td>
